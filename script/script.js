c6a1c462849533ce94b1a5648cd25e7d1fbe0cbd
ray-pH <rayhan.azizi9@gmail.com>
2022-12-26 01:34:04 +0700

    add scenes.ts

--- script/script.js
+++ script/script.js
@@ -1,31 +1,21 @@
 import { QParticle, QRenderer } from "./quantumMech.js";
+import { scene_Parabola } from "./scenes.js";
 var canvas = document.getElementById("canvas");
 var n = 200;
+var ny = 300; // for canvas
 var dt = 1e-6;
 // approximately dψ_re <= 2Δt/Δx^2 = 2Δt n^2
 // to make it stable set at most Δt < δψ/n^2
-//
 var qparticle = new QParticle(n, dt);
-var qrenderer = new QRenderer(qparticle, canvas, 300);
+var qrenderer = new QRenderer(qparticle, canvas, ny);
 var potentArr = new Float64Array(n);
-for (let i = 0; i < n; i++) {
-    let x = i / n;
-    potentArr[i] = n * n * (x - 0.5) * (x - 0.5);
-}
 var realArr = new Float64Array(n);
 var imagArr = new Float64Array(n);
-var sinusoidscale = 40;
-for (let i = 0; i < n; i++) {
-    let x = i / n;
-    let y = -(x - 0.1) * (x - 0.4);
-    let envelope = Math.max(y, 0);
-    realArr[i] = envelope * Math.sin(sinusoidscale * x);
-    imagArr[i] = envelope * Math.cos(sinusoidscale * x);
-}
+scene_Parabola(potentArr, realArr, imagArr);
+qparticle.setPotential(potentArr);
 qparticle.Psi.setReal(realArr);
 qparticle.Psi.setImag(imagArr);
 qparticle.Psi.setPeak(0.8);
-qparticle.setPotential(potentArr);
 qrenderer.setVjmax(0.8);
 qrenderer.option_drawBottomPot = false;
 var paused = false;
---
+++ src/scenes.ts
@@ -0,0 +1,39 @@
+function scene_Parabola(potentArr : Float64Array, realArr : Float64Array, imagArr : Float64Array, 
+                        energy : number = 40) : void{
+    let n = potentArr.length;
+
+    for (let i = 0; i < n; i++) {
+        let x = i / n;
+        let y = -(x - 0.1) * (x - 0.4);
+        let envelope = Math.max(y, 0);
+        realArr[i] = envelope * Math.sin(energy * x);
+        imagArr[i] = envelope * Math.cos(energy * x);
+    }
+
+    for (let i = 0; i < n; i++){
+        let x = i/n;
+        potentArr[i] = n * n * (x-0.5) * (x-0.5);
+    }
+}
+
+function scene_Tunneling(potentArr : Float64Array, realArr : Float64Array, imagArr : Float64Array,
+                        energy : number = 60) : void{
+    let n = potentArr.length;
+
+    for (let i = 0; i < n; i++) {
+        let x = i / n;
+        let y = -(x - 0.7) * (x - 0.9);
+        let envelope = Math.max(y, 0);
+        realArr[i] = envelope * Math.sin(energy * x);
+        imagArr[i] = envelope * Math.cos(energy * x);
+    }
+
+    let ifrom = Math.round(0.55 * n);
+    let ito   = Math.round(0.60 * n);
+    potentArr.fill(0.0);
+    for (let i = ifrom; i < ito; i++){
+        potentArr[i] = n * n * 0.05;
+    }
+}
+
+export {scene_Parabola, scene_Tunneling};
