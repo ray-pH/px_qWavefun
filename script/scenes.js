54d5c8abaf7f727cacdffaa3d708b90b31a32ef6
ray-pH <rayhan.azizi9@gmail.com>
2022-12-30 01:34:22 +0700

    change xxxArr to xxx in scenes

--- script/scenes.js
+++ script/scenes.js
@@ -5,41 +5,41 @@ function scene_set(qp, sf) {
     sf(potentArr, realArr, imagArr);
 }
 function strScene_toFun(s) {
-    let f = new Function('potentArr', 'realArr', 'imagArr', "\"use strict\";\n" + s);
+    let f = new Function('potent', 'real', 'imag', "\"use strict\";\n" + s);
     return f;
 }
-let strScene_Parabola = `let n = potentArr.length;
+let strScene_Parabola = `let n = potent.length;
 let energy = 40;
 
 for (let i = 0; i < n; i++) {
     let x = i / n;
     let y = -(x - 0.1) * (x - 0.4);
     let envelope = Math.max(y, 0);
-    realArr[i] = envelope * Math.sin(energy * x);
-    imagArr[i] = envelope * Math.cos(energy * x);
+    real[i] = envelope * Math.sin(energy * x);
+    imag[i] = envelope * Math.cos(energy * x);
 }
 
 for (let i = 0; i < n; i++){
     let x = i/n;
-    potentArr[i] = n * n * (x-0.5) * (x-0.5);
+    potent[i] = n * n * (x-0.5) * (x-0.5);
 }
 `;
-let strScene_Tunneling = `let n = potentArr.length;
+let strScene_Tunneling = `let n = potent.length;
 let energy = 60;
 
 for (let i = 0; i < n; i++) {
     let x = i / n;
     let y = -(x - 0.7) * (x - 0.9);
     let envelope = Math.max(y, 0);
-    realArr[i] = envelope * Math.sin(energy * x);
-    imagArr[i] = envelope * Math.cos(energy * x);
+    real[i] = envelope * Math.sin(energy * x);
+    imag[i] = envelope * Math.cos(energy * x);
 }
 
 let ifrom = Math.round(0.55 * n);
 let ito   = Math.round(0.60 * n);
-potentArr.fill(0.0);
+potent.fill(0.0);
 for (let i = ifrom; i < ito; i++){
-    potentArr[i] = n * n * 0.05;
+    potent[i] = n * n * 0.05;
 }
 `;
 export { scene_set, strScene_toFun, strScene_Parabola, strScene_Tunneling };
--- src/scenes.ts
+++ src/scenes.ts
@@ -10,45 +10,45 @@ function scene_set(qp : QParticle, sf : scenefun){
 }
 
 function strScene_toFun(s : string) : scenefun {
-    let f : scenefun = new Function('potentArr', 'realArr', 'imagArr', "\"use strict\";\n" + s) as scenefun;
+    let f : scenefun = new Function('potent', 'real', 'imag', "\"use strict\";\n" + s) as scenefun;
     return f;
 }
 
 let strScene_Parabola : string = 
-`let n = potentArr.length;
+`let n = potent.length;
 let energy = 40;
 
 for (let i = 0; i < n; i++) {
     let x = i / n;
     let y = -(x - 0.1) * (x - 0.4);
     let envelope = Math.max(y, 0);
-    realArr[i] = envelope * Math.sin(energy * x);
-    imagArr[i] = envelope * Math.cos(energy * x);
+    real[i] = envelope * Math.sin(energy * x);
+    imag[i] = envelope * Math.cos(energy * x);
 }
 
 for (let i = 0; i < n; i++){
     let x = i/n;
-    potentArr[i] = n * n * (x-0.5) * (x-0.5);
+    potent[i] = n * n * (x-0.5) * (x-0.5);
 }
 `
 
 let strScene_Tunneling : string = 
-`let n = potentArr.length;
+`let n = potent.length;
 let energy = 60;
 
 for (let i = 0; i < n; i++) {
     let x = i / n;
     let y = -(x - 0.7) * (x - 0.9);
     let envelope = Math.max(y, 0);
-    realArr[i] = envelope * Math.sin(energy * x);
-    imagArr[i] = envelope * Math.cos(energy * x);
+    real[i] = envelope * Math.sin(energy * x);
+    imag[i] = envelope * Math.cos(energy * x);
 }
 
 let ifrom = Math.round(0.55 * n);
 let ito   = Math.round(0.60 * n);
-potentArr.fill(0.0);
+potent.fill(0.0);
 for (let i = ifrom; i < ito; i++){
-    potentArr[i] = n * n * 0.05;
+    potent[i] = n * n * 0.05;
 }
 `
 
